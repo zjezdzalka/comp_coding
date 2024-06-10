5a801d3583d8d81c5a06bcfe7157dcaee7c0b220
Marek Zjeżdżałka <UnderscoreF17@gmail.com>
2024-06-11 00:53:53 +0200

    Create ci2.cpp
    to fix, didnt have a concept, need to think later on

---
+++ Kolo OI/OKI_Wakacje_2024/ci2.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int n,m;
+	string wzorzec;
+	string ciasto;
+	cin>>n>>m;
+	cin>>wzorzec>>ciasto;
+	int i;
+	/* do naprawienia */
+	return 0;
+}
