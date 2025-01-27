91b0281f19e91034c7d0bb33a652f41fc8ea615f
Marek Zjeżdżałka <UnderscoreF17@gmail.com>
2025-01-27 10:41:48 +0100

    sta finished

--- OKI_ZSK_24_25/sta.cpp
+++ OKI_ZSK_24_25/sta.cpp
@@ -1,20 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+bool isPalindrom(string x){
+	int i;
+	for(i = 0; i<x.length()/2; ++i){
+		if(x[i] != x[x.length()-i-1]) return false;
+	}
+	
+	if(x.length() == 0) return false;
+	return true;
+}
+
 int main(){
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 	
-	string n;
-	cin>>n;
-	
 	bool isNum = false;
-	int spc_counter = 0;
-	int num_counter = 0;
-	int word_counter = 0;
-	int sen_counter = 0;
-	int pal_counter = 0;
+	bool isWord = false;
+	bool isSentence = false;
+	bool isPali = false;
+	
+	// space, num, word, sentence, palindrom
+	vector<int> vals = {0,0,0,0,0};
 	
 	int i;
 	
@@ -22,29 +30,92 @@ int main(){
 	cin>>x;
 	int temp;
 	
-	stack<int> nums;
+	queue<int> nums;
 	
 	for(i=0;i<x;++i){
 		cin>>temp;
 		nums.push(temp);
 	}
 	
+	string n;
+	cin.ignore();
+	getline(cin, n);
+	
+	string word = "";
+	
 	for(i=0;i<n.length();++i){
-		if(n[i] == '  '){
-			spc_counter += 1;
-			if(isNum) num_counter += 1;
+		if(n[i] == ' '){
+			vals[0] += 1;
+			
+			if(isNum) vals[1] += 1;
 			isNum = false;
+			
+			if(isWord) vals[2] += 1;
+			isWord = false;
+			isPali = false;
+			
+			if(isPalindrom(word)) vals[4] += 1;
+			word = "";
 		}
 		else if(n[i] >= '0' && n[i] <= '9'){
 			isNum = true;
+			
+			if(isWord) vals[2] += 1;
+			isWord = false;
+			isPali = false;
+			
+			if(isPalindrom(word)) vals[4] += 1;
+			word = "";
+		}
+		else if(n[i] >= 'a' && n[i] <= 'z' || n[i] >= 'A' && n[i] <= 'Z'){
+			if(n[i] >= 'A' && n[i] <= 'Z'){
+				n[i] += 32;
+			}
+			
+			isWord = true;
+			
+			word += string(1, n[i]);
+			
+			if(isNum) vals[1] += 1;
+			isNum = false;
+			
+			isSentence = true;
+		}
+		else if(n[i] == '.'){
+			if(isNum) vals[1] += 1;
+			isNum = false;
+			
+			if(isWord) vals[2] += 1;
+			isWord = false;
+			isPali = false;
+			
+			if(isSentence) vals[3] += 1;
+			isSentence = false;
+			
+			if(isPalindrom(word)) vals[4] += 1;
+			word = "";
 		}
 		else{
-			if(isNum) num_counter += 1;
+			if(isNum) vals[1] += 1;
 			isNum = false;
+			
+			if(isWord) vals[2] += 1;
+			isWord = false;
+			isPali = false;
+
+			if(isPalindrom(word)) vals[4] += 1;
+			word = "";
 		}
 	}
 	
-	cout<<spc_counter<<" "<<num_counter<<" "<<word_counter<<" "<<sen_counter<<" "<<pal_counter<<"\n";
+	if(isNum) vals[1] += 1;
+	if(isWord) vals[2] += 1;
+	if(isPalindrom(word)) vals[4] += 1;
+	
+	while(!nums.empty()){
+		cout<<vals[nums.front()-1]<<" ";
+		nums.pop();
+	}
 	
 	return 0;
 }
