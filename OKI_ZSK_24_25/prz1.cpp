6d3d38083dbf2c6a0fc46ccf547d4edae63753c9
Marek Zjeżdżałka <UnderscoreF17@gmail.com>
2025-01-29 09:09:05 +0100

    failed attempt at 100
    cant get from 97 -> 100, pmo

--- OKI_ZSK_24_25/prz1.cpp
+++ OKI_ZSK_24_25/prz1.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<algorithm>
-#include<vector>
+
+int values[1000002];     // 1 <= n <= 10^6
 
 int main(){
 	std::ios_base::sync_with_stdio(false);
@@ -12,24 +13,17 @@ int main(){
 	
 	int i, maks = -1, num, add = 0;
 	
-	std::vector<int> values(n);
-	
 	for(i=0;i<m;++i){
 		std::cin>>num;
 		
-		num -= 1;
-		
-		if(num != n){
-			values[num] = std::max(values[num]+1, add+1);
-			maks = std::max(values[num], maks);
-			continue;
-		}
-		
-		add = maks;
+		values[num] = values[num]+1 > add+1 ? values[num]+1 : add+1;
+
+		if(num != n+1) maks = values[num] > maks ? values[num] : maks;
+		else add = maks;
 	}
 	
-	for(i=0;i<n;++i){
-		std::cout<<std::max(add, values[i])<<" ";
+	for(i=1;i<=n;++i){
+		std::cout<<(add > values[i] ? add : values[i])<<" ";
 	}
 	
 	return 0;
