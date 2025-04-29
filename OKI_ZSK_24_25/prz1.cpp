d2108922834b78af7afc03f722148ef9b7d04bcf
Marek Zjeżdżałka <UnderscoreF17@gmail.com>
2025-04-29 21:16:39 +0200

    97 -> 97
    bitset??

--- OKI_ZSK_24_25/prz1.cpp
+++ OKI_ZSK_24_25/prz1.cpp
@@ -1,25 +1,30 @@
 #include<iostream>
 
+int values[1000002];  
+
 int main(){
 	std::ios_base::sync_with_stdio(false);
 	std::cin.tie(NULL);
-	std::cout.tie(NULL);
 	
 	int n,m;
 	std::cin>>n>>m;
-
-	int values[n+2];     // 1 <= n <= 10^6
-
-	std::fill(values, values+n+2, 0);
 	
-	int maks = -1, num, add = 0;
+	int maks = 0, num, add = 0;
 	
 	while(m--){
 		std::cin>>num;
 		
-		values[n-num] = values[n-num]+1 > add+1 ? values[n-num]+1 : add+1;
+		int pos = n-num;
 
-		(num != n+1) ? maks = (values[n-num] > maks ? values[n-num] : maks) : add = maks;
+		values[pos] = values[pos] > add ? values[pos]+1 : add+1;
+
+		if (num != n+1) {
+			if (values[pos] > maks) 
+				maks = values[pos];
+		} 
+		else {
+			add = maks;
+		}	
 	}
 	
 	while(n--){
